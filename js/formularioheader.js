c033580133ee5bddf4fdd195053e89da752b8807
valentina-paz <vpaz@udesa.edu.ar>
2023-05-30 12:47:39 -0300

    arreglamos el js del buscador

--- js/formularioheader.js
+++ js/formularioheader.js
@@ -1,18 +1,13 @@
-// dcapturar form y ejecutar submmit adentro prevent defaault. capturo input (querySelector) . value IF y voy metiendo ifs git
-let form= document.querySelector("#buscador")
+let form = document.querySelector("#buscador")
 let input = document.querySelector("#busqueda")
-let valor= input.value
-form.addEventListener("submit", function(e){
-    e.preventDefault()
-    if(valor == 0){
-        alert ("El campo esta vacío, ingrese una búsqueda!")
-    }else if(valor > 1 && valor < 3){
-        alert ("El campo debe contener al menos 3 caracteres")
-    }else{
-        window.location.href= "../search-results.html"
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let valor= input.value.length
+    if (valor == 0) {
+        alert("El campo esta vacío, ingrese una búsqueda!")
+    } else if (valor < 3) {
+        alert("El campo debe contener al menos 3 caracteres")
+    } else {
+        this.submit(); //preguntar. pusimos esto porque window.location.href= "../search-results.html" no funcionaba
     }
-    
-})
-
-
-// declaramos id de form e input y capturamos los valores. Hicimos el js del heather.
+});
