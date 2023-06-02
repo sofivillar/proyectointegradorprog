9d2850a9400463b4e7dc4480af25b3f9399e97c5
valentina-paz <vpaz@udesa.edu.ar>
2023-06-02 15:13:20 -0300

    borramos todos los articles para que la api los haga después

--- js/index.js
+++ js/index.js
@@ -12,7 +12,6 @@ fetch (urlHome)
         console.log(arrayCanciones);
         let listaCanciones= document.querySelector(".nombre-canciones");
         let canciones=""
-
         for(let i=0; i<arrayCanciones.length; i++){
             canciones += `<article class= "bloque-cancion"> <h3> <a class="nombre-cancion" href="./detallecancion.html?COMPLETARQS">Nombre de la cancion</a></h3>
                 <img src="${arrayCanciones[i].link}" alt="${arrayCanciones[i].title}"> 
