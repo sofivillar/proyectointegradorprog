c64623b1c00cc92547e29ee95c8209f372cd500e
valentina-paz <vpaz@udesa.edu.ar>
2023-06-09 15:44:11 -0300

    editando js

--- js/detalledisco.js
+++ js/detalledisco.js
@@ -1,33 +1,49 @@
 let qs= location.search;
 let qsToObject = new URLSearchParams(qs);
 let disco= qsToObject.get('id');
+
 let nombredisco= document.querySelector(".nombre-disco")
 let titulo= document.querySelector(".titulo-detalle-d")
-
-let tituloResultados = document.querySelector("h1")
-tituloResultados.innerText += `${disco.title}` 
+let contenedorDisco= document.querySelector(".contenedor-detalle-disco");
 
 let url = `https://api.allorigins.win/raw?url=https://api.deezer.com/album/${disco}`
+let urlCanciones= `https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/${disco}/tracks`
+
 fetch(url)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data);
-        let contenedorDisco= document.querySelector(".contenedor-detalle-disco");
-        let discoDatos = `<article class= "detalle-disco"> <h3> <a class="nombre-disco" href="./detalledisco.html?id=${data.id}">${data.title}</a></h3>
-                <img src="${data.cover}" alt="${data.title}"> 
-                <article class="bloque-disco-datos">
-                    <a href="./detallesartista.html?id=${data.artist.name}">${data.artist.name}</a>  
-                    <a href="./genres.html?id=${data}
-                    <p> Fecha de publicacion del disco: ${data.release_date}</p>
-                    <p> Lista de canciones del artista: ${data.artist.tracklist}</p>
-                </article>
-        </article>`
-        contenedorDisco.innerHTML=  `${discoDatos}` 
+        let discoDatos = `<article class= "detalle-disco"> <a class="nombre-disco" href="./detalledisco.html?id=${data.id}">${data.title}</a> <img src="${data.cover}" alt="${data.title}"> <article class="bloque-disco-datos"> <a href="./detallesartista.html?id=${data.artist.id}">${data.artist.name}
+        </a> <a href="./detail-genres.html?id=${data.genres.data.id}>${data.genres.data.name}</a><p> Fecha de publicacion del disco: ${data.release_date}</p></article>`
+        contenedorDisco.innerHTML= `${discoDatos}` 
         titulo.innerText= `Detalles del album: ${data.title}`
-
+        
     })
 .catch(function(e){
     console.log(e);
+})
+
+
+
+
+fetch (urlCanciones)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data)
+    let arrayCanciones= data.data;
+    console.log(arrayCanciones); 
+    let canciones=""
+    for(let i=0; i< 5; i++){
+        canciones += `<ul class="lista-detalle-disco"> <li> <a class="detalle-cancion" href="./detallecancion.html?id=${arrayCanciones[i].data.id}">${arrayCanciones[i].data.title}</a> </li> </ul> 
+        </article>`
+    }
+    contenedorDisco.innerHTML += canciones
+    
+})
+.catch(function(error){
+    console.log( "Error: " + error);
 })
