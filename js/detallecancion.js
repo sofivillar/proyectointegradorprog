f5dac8cd257e8ab4deca64ff4b37202bb62559ed
valentina-paz <vpaz@udesa.edu.ar>
2023-06-09 01:05:29 -0300

    terminamos de hacer el detalle de cancion

--- js/detallecancion.js
+++ js/detallecancion.js
@@ -1,18 +1,30 @@
 let qs= location.search;
 let qsToObject = new URLSearchParams(qs);
 let cancion= qsToObject.get('id');
+let nombreCancion= document.querySelector(".nombre-cancion")
+let titulo= document.querySelector(".titulo-detalle-c")
 
 let tituloResultados = document.querySelector("h1")
-tituloResultados.innerText += ` ${cancion}`
+tituloResultados.innerText += `${cancion.title}` 
 
-let url =`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/?id=${cancion}`
+let url =`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${cancion}`
 fetch(url)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data);
-       
+        let contenedorCancion= document.querySelector(".contenedor-detalle-canciones");
+        let cancion = `<article class= "bloque-cancion"> <h3> <a class="nombre-cancion" href="./detallecancion.html?id=${data.id}">${data.title}</a></h3>
+                <img src="${data.album.cover}" alt="${data.title}"> 
+                <article class="bloque-cancion-datos">
+                    <a href="./detalledisco.html?id=${data.album.title}">${data.album.title}</a>
+                    <a href="./detallesartista.html?id=${data.artist.name}">${data.artist.name}</a>  
+                </article>
+        </article>`
+        contenedorCancion.innerHTML=  `${cancion}` 
+        titulo.innerText= `Detalles de la canción: ${data.title}`
+
     })
 .catch(function(e){
     console.log(e);
