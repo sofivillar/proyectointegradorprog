ed55852e35ec8288cda4ff6216ce87604507427a
valentina-paz <vpaz@udesa.edu.ar>
2023-06-09 16:09:27 -0300

    detalle disco

--- js/detalledisco.js
+++ js/detalledisco.js
@@ -15,9 +15,17 @@ fetch(url)
     })
     .then(function(data){
         console.log(data);
-        let discoDatos = `<article class= "detalle-disco"> <a class="nombre-disco" href="./detalledisco.html?id=${data.id}">${data.title}</a> <img src="${data.cover}" alt="${data.title}"> <article class="bloque-disco-datos"> <a href="./detallesartista.html?id=${data.artist.id}">${data.artist.name}
-        </a> <a href="./detail-genres.html?id=${data.genres.data.id}>${data.genres.data.name}</a><p> Fecha de publicacion del disco: ${data.release_date}</p></article>`
-        contenedorDisco.innerHTML= `${discoDatos}` 
+        let discoDatos = `<article class= "detalle-disco">
+                             <a class="nombre-disco" href="./detalledisco.html?id=${data.id}">${data.title}</a> <img src="${data.cover}" alt="${data.title}">
+                             <a href="./detallesartista.html?id=${data.artist.id}">sfs${data.artist.name}</a>
+                             adfsfsdfsdfsdfs
+                             sdfsdf
+                             sdfsdf
+                             sdfsdfsd
+                             <a href="./detail-genres.html?id=${data.genres.data[0].id}>${data.genres.data[0].name}</a>
+                             <p>Fecha de publicacion del disco:${data.release_date}</p>
+                            </article>`
+        contenedorDisco.innerHTML= discoDatos 
         titulo.innerText= `Detalles del album: ${data.title}`
         
     })
@@ -28,22 +36,22 @@ fetch(url)
 
 
 
-fetch (urlCanciones)
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data)
-    let arrayCanciones= data.data;
-    console.log(arrayCanciones); 
-    let canciones=""
-    for(let i=0; i< 5; i++){
-        canciones += `<ul class="lista-detalle-disco"> <li> <a class="detalle-cancion" href="./detallecancion.html?id=${arrayCanciones[i].data.id}">${arrayCanciones[i].data.title}</a> </li> </ul> 
-        </article>`
-    }
-    contenedorDisco.innerHTML += canciones
+// fetch (urlCanciones)
+// .then(function(response){
+//     return response.json();
+// })
+// .then(function(data){
+//     console.log(data)
+//     let arrayCanciones= data.data;
+//     console.log(arrayCanciones); 
+//     let canciones=""
+//     for(let i=0; i< 5; i++){
+//         canciones += `<ul class="lista-detalle-disco"> <li> <a class="detalle-cancion" href="./detallecancion.html?id=${arrayCanciones[i].data.id}">${arrayCanciones[i].data.title}</a> </li> </ul> 
+//         </article>`
+//     }
+//     contenedorDisco.innerHTML += canciones
     
-})
-.catch(function(error){
-    console.log( "Error: " + error);
-})
+// })
+// .catch(function(error){
+//     console.log( "Error: " + error);
+// })
