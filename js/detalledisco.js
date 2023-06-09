71d7afd0eb89a0b793847ff91ad79a803ae848eb
sofivillar <villars@udesa.edu.ar--global>
2023-06-09 16:50:21 -0300

    Terminamos el detalle del disco

--- js/detalledisco.js
+++ js/detalledisco.js
@@ -1,13 +1,14 @@
 let qs= location.search;
 let qsToObject = new URLSearchParams(qs);
 let disco= qsToObject.get('id');
+let li= document.querySelector(".li");
 
 let nombredisco= document.querySelector(".nombre-disco")
 let titulo= document.querySelector(".titulo-detalle-d")
 let contenedorDisco= document.querySelector(".contenedor-detalle-disco");
 
 let url = `https://api.allorigins.win/raw?url=https://api.deezer.com/album/${disco}`
-let urlCanciones= `https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/${disco}/tracks`
+
 
 fetch(url)
     .then(function(response){
@@ -15,13 +16,22 @@ fetch(url)
     })
     .then(function(data){
         console.log(data);
+        let canciones=""
+        let arrayCanciones=data.tracks.data
+        for(i=0; i<arrayCanciones.length; i++){
+            //canciones+= arrayCanciones[i].title+" "
+            canciones += "<li>" + arrayCanciones[i].title + "</li>"
+        }
+        console.log(canciones)
         let discoDatos = `<article class="detalle-disco">
                              <a class="nombre-disco" href="./detalledisco.html?id=${data.id}">${data.title}</a> <img src="${data.cover}" alt="${data.title}">
                              <a href="./detallesartista.html?id=${data.artist.id}">${data.artist.name}</a>
                              <a href="./detail-genres.html?id=${data.genres.data[0].id}">${data.genres.data[0].name}</a>
                              <p>Fecha de publicación del disco: ${data.release_date}</p>
+                          </article>
+                          <ul> ${canciones}</ul>
+                          `
 
-                          </article>`
         contenedorDisco.innerHTML= discoDatos 
         titulo.innerText= `Detalles del album: ${data.title}`
         
