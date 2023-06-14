92e6bdc364d0cf4036ea2fa046ca4c09d8f26b6c
valentina-paz <vpaz@udesa.edu.ar>
2023-06-14 18:43:59 -0300

    boton favoritos y guardado en local storage

--- js/detallecancion.js
+++ js/detallecancion.js
@@ -1,47 +1,59 @@
-let qs= location.search;
+let qs = location.search;
 let qsToObject = new URLSearchParams(qs);
-let cancion= qsToObject.get('id');
-let nombreCancion= document.querySelector(".nombre-cancion")
-let titulo= document.querySelector(".titulo-detalle-c")
+let id = qsToObject.get('id');
+console.log(id)
+let nombreCancion = document.querySelector(".nombre-cancion")
+let titulo = document.querySelector(".titulo-detalle-c")
 
-let tituloResultados = document.querySelector("h1")
-tituloResultados.innerText += `${cancion.title}` 
-
-let url =`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${cancion}`
+let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${id}`
 fetch(url)
-    .then(function(response){
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
         console.log(data);
-        let contenedorCancion= document.querySelector(".contenedor-detalle-canciones");
+        let contenedorCancion = document.querySelector(".contenedor-detalle-canciones");
         let cancion = `<article class= "bloque-cancion"> <h3> <a class="nombre-cancion" href="./detallecancion.html?id=${data.id}">${data.title}</a></h3>
                 <img src="${data.album.cover}" alt="${data.title}"> 
                 <article class="bloque-cancion-datos">
                    <a href="./detalledisco.html?id=${data.album.id}">${data.album.title}</a>
                    <a href="./detallesartista.html?id=${data.artist.id}">${data.artist.name}</a> 
                 </article>
-                <form action="./playlist.html" method="GET">
-                <button id="favs" type="submit">Añadir a favoritos</button>
-                </form>
         </article>`
-        contenedorCancion.innerHTML=  cancion 
-        titulo.innerText= `Detalles de la canción: ${data.title}`
+        contenedorCancion.innerHTML += cancion
+        titulo.innerText = `Detalles de la canción: ${data.title}`
 
     })
-.catch(function(e){
-    console.log(e);
+    .catch(function (e) {
+        console.log(e);
+    })
+
+let botonFavs = document.querySelector('.favs');
+let playlist = [];
+let recuperoStorage = localStorage.getItem('playlist');
+let storageToArray= JSON.parse(recuperoStorage)
+
+if (recuperoStorage != null) {
+    playlist = storageToArray;
+}
+
+if (playlist.includes(id)) {
+    botonFavs.innerText = 'Quitar de Favoritos'
+}
+
+botonFavs.addEventListener("click", function () {
+    if (playlist.includes(id)) {
+        let indice = playlist.indexOf(id)
+        playlist.splice(indice, 1);
+        botonFavs.innerText = 'Agregar a favorito'
+    } else {
+        playlist.push(id);
+        botonFavs.innerText = 'Quitar de favorito'
+    }
+
+    let favsToString = JSON.stringify(playlist);
+    localStorage.setItem('playlist', favsToString)
 })
 
-let cancionFav= [];
-let botonFavs= document.querySelector("#favs");
 
-botonFavs.addEventListener("click", function(){
-    cancionFav.push(cancion);
-    cancionToJson = JSON.stringify(cancionFav);
-    localStorage.setItem("listaFavoritos", cancionToJson) 
-    console.log(localStorage);
-})
-
-//no se guarda nada en el local.storage. ale lo hace con una <a> en vez de con button. chequear y terminar
 
